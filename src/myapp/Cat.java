3374d3f76b5b1c155fea78570f1987b89bd74866
Kritsada <kritsada.kr@ku.th>
2022-07-10 08:12:02 +0700

    Update Cat.java by feature2

--- src/myapp/Cat.java
+++ src/myapp/Cat.java
@@ -2,8 +2,10 @@ package myapp;
 
 public class Cat {
     private String name;
+    private int age;
 
-    public Cat(String name) {
+    public Cat(String name,int age) {
         this.name = name;
+        this.age = age;
     }
 }
